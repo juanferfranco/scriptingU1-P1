361d097dc09b507a847c10b1a8f116dfcaee80a3
Juan Franco <juanferfranco@gmail.com>
2021-01-27 11:19:59 -0500

    Ensalkjsfksfdkjhasdf

--- Assets/Scripts/Test1.cs
+++ Assets/Scripts/Test1.cs
@@ -7,12 +7,13 @@ public class Test1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        asdkfhjaskdhfaksdhfkajsdhfksajdhfkdsajh
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        sdfkhslfdkhasdlkfhjasd
+        asfdkjhasdflkjhasdflkhasdflkjhasdkfjhass
     }
 }
